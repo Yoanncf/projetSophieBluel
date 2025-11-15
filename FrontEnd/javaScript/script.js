1fc10a6a5442450d0a87429fd7db9ec26debb394
Yoann Nicolai <yoann.nicolai@connectingflow.com>
2025-11-15 18:07:41 +0100

    Ajout de la fonction pour récupérer dynamiquement les catégories depuis l'API

--- FrontEnd/javaScript/script.js
+++ FrontEnd/javaScript/script.js
@@ -1,6 +1,7 @@
 // création des variables
 const gallery = document.querySelector('.gallery');
 const API = 'http://localhost:5678/api/works';
+const API_CATEGORIES = 'http://localhost:5678/api/categories';
 let allWorks = [];
 
 //Appel à l’API avec fetch afin de récupérer dynamiquement les projets de l’architecte. 
@@ -44,3 +45,19 @@ const figureWork = (work) => {
     return figure;
 }
 
+
+
+const fetchAllCategories = async () => {
+   try {
+    const result = await fetch(`${API_CATEGORIES}`);
+    if(!result.ok){
+        console.error('Erreur API');
+    }
+    const data = await result.json();
+    console.log(data);
+   } catch(error) {
+    console.error("Erreur lors de la récupération de tous les catégories")
+   }
+   
+}
+fetchAllCategories();
